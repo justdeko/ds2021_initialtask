90a08dbc690eaefb54a0e34d2ce67a97a117bbf7
Denis Koljada <denis@koljada.de>
2021-05-12 14:49:45 +0200

    updated whisk trigger stopdate

--- task1/create_functions.sh
+++ task1/create_functions.sh
@@ -7,7 +7,7 @@ wsk -i trigger create interval \
   --feed /whisk.system/alarms/interval \
   --param minutes 5 \
   --param trigger_payload "{\"city\":\"Berlin\"}" \
-  --param stopDate "2021-05-10T23:59:00.000Z"
+  --param stopDate "2021-05-13T23:59:00.000Z"
 # check updates, create rule that connects umbrellor to periodic trigger
 wsk -i trigger update interval
 wsk -i action update umbrellor umbrellor.py
